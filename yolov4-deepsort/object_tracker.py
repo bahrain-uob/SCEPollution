7795ab4fde7506c940b3cbf52832fcd906b1ede8
Abdullatif Rashdan <abdullatif.rashdan@gmail.com>
2022-04-19 16:37:26 +0300

    GPU memory growth

--- yolov4-deepsort/object_tracker.py
+++ yolov4-deepsort/object_tracker.py
@@ -40,7 +40,7 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     try:
         for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpus, True)
+            tf.config.experimental.set_memory_growth(gpu, True)
     except RuntimeError as e:
         print(e)
 
