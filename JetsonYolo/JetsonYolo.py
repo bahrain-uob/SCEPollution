05afb0f5f143f87112002414a5566aea5d0406f5
Reema Khairalla <reema.khairalla@gmail.com>
2022-04-26 17:18:15 +0300

    new tracker code

--- JetsonYolo/JetsonYolo.py
+++ JetsonYolo/JetsonYolo.py
@@ -2,6 +2,13 @@ import time
 import cv2
 import numpy as np
 from elements.yolo import OBJ_DETECTION
+# deep sort imports
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+from application_util import preprocessing
+from application_util import visualization
 
 Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
                 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
@@ -16,6 +23,18 @@ Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', '
 Object_colors = list(np.random.rand(80,3)*255)
 Object_detector = OBJ_DETECTION('weights/yolov5s.pt', Object_classes)
 
+# Definition of the parameters
+max_cosine_distance = 0.4
+nn_budget = None
+nms_max_overlap = 1.0
+# initialize deep sort
+model_filename = 'model_data/mars-small128.pb'
+encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+# calculate cosine distance metric
+metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+# initialize tracker
+tracker = Tracker(metric)
+
 # begin video capture
 video_path="./cars.mp4"
 try:
@@ -24,18 +43,21 @@ except:
     vid = cv2.VideoCapture(video_path)
 out = None
 
-print('start objecct deection')
+print('start object detection')
 # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
 while vid.isOpened():
     return_value, frame = vid.read()
     if return_value:
         start_time = time.time()
-        objs = Object_detector.detect(frame)
-        for obj in objs:
+        detections = Object_detector.detect(frame)
+        scores = np.array([d.score for d in detections])
+        boxes = 
+        for obj in detections:
             # print(obj)
             label = obj['label']
             score = obj['score']
-            [(xmin,ymin),(xmax,ymax)] = obj['bbox']
+            # [(xmin,ymin),(xmax,ymax)] = obj['bbox']
+            print(obj['bbox'])
             color = Object_colors[Object_classes.index(label)]
             # frame = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), color, 2) 
             # frame = cv2.putText(frame, f'{label} ({str(score)})', (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX , 0.75, color, 1, cv2.LINE_AA)
