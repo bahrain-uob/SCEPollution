21bcc6e245b6f879ad015bf59c103df673e6a3f0
Reema Khairalla <reema.khairalla@gmail.com>
2022-05-07 16:30:57 +0300

    new code

--- yolov4-deepsort/deep_sort/linear_assignment.py
+++ yolov4-deepsort/deep_sort/linear_assignment.py
@@ -60,12 +60,12 @@ def min_cost_matching(
     indices = np.transpose(indices)
     matches, unmatched_tracks, unmatched_detections = [], [], []
     for col, detection_idx in enumerate(detection_indices):
-        if col not in indices[:, 1]:
+        if col not in indices[1]:
             unmatched_detections.append(detection_idx)
     for row, track_idx in enumerate(track_indices):
-        if row not in indices[:, 0]:
+        if row not in indices[0]:
             unmatched_tracks.append(track_idx)
-    for row, col in indices:
+    for row, col in list(zip(indices[0], indices[1])):
         track_idx = track_indices[row]
         detection_idx = detection_indices[col]
         if cost_matrix[row, col] > max_distance:
