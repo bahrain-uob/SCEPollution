77ade470b6c828775b3c3e59c0ddd212983b0429
Reema Khairalla <reema.khairalla@gmail.com>
2022-05-10 17:29:42 +0300

    new code

--- pytorchYolo-container/runDocker.sh
+++ pytorchYolo-container/runDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker build -t detector .
-sudo docker run -it --rm --runtime nvidia --network host -v /home/trafficpollution/repo/SCEPollution:/home detector  -e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix  # <--- Here
+sudo docker run -it --rm --runtime nvidia --network host -v /home/trafficpollution/repo/SCEPollution:/home detector -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  # <--- Here
