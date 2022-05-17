bacde45ffc08097e2478d5c5e78152ee0d7643a3
Abdullatif <arashdan@amazon.com>
2022-05-17 17:42:00 +0300

    Code

--- JetsonYolo/JetsonYolo.py
+++ JetsonYolo/JetsonYolo.py
@@ -122,125 +122,132 @@ wait_frame_count = {}
 WarmUpCount = 0 
 fpscv2 = vid.get(cv2.CAP_PROP_FPS)
 
+while True:
 # start forever loop 
-while vid.isOpened():
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    return_value, frame = vid.read()
-    if return_value:
-        start_time = time.time()
-        detections = Object_detector.detect(frame)
-        if WarmUpCount < 9:
-            WarmUpCount += 1 
-            continue
-        if start:
-            total_start_time = time.time() 
-            start = False
-        boxes = [] 
-        labels = []
-        scores = []
-
-        for obj in detections:
-            if obj['label'] in allowed_classes and obj['score'] > 0.5:
-                [(xmin,ymin),(xmax,ymax)] = obj['bbox']
-                h = ymax - ymin
-                w = xmax - xmin
-                boxes.append((xmin,ymin, w, h))
-                labels.append(obj['label'])
-                scores.append(obj['score'])
-
-        boxes = np.array(boxes)
-        labels = np.array(labels)
-        scores = np.array(scores)
-
-        # Start non max supression 
-        features = encoder(frame, boxes)
-        detections = [Detection(box, score, label, feature) for box, score, label, feature in zip(boxes, scores, labels, features)]
-        boxs = np.array([d.tlwh for d in detections])
-        scores = np.array([d.confidence for d in detections])
-    # run non-maxima supression
-        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
-        detections = np.array([detections[i] for i in indices])       
-        # Call the tracker
-        tracker.predict()
-        tracker.update(detections)
-
-        # update tracks
-        cars = 0
-        trucks = 0
-        busses = 0
-        print('Objects being tracked: {}'.format(str(len(tracker.tracks))))        
-        for track in tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
-            bbox = track.to_tlbr()
-            class_name = track.get_class()
-            if class_name == 'car':
-                cars += 1
-            elif class_name == 'truck':
-                trucks += 1
-            elif class_name == 'bus':
-                busses += 1
-
-            #counting frames for each track
-            trackID=str(track.track_id)
-            if trackID in wait_frame_count.keys():
-                wait_frame_count[trackID] += 1   
-            else:
-                wait_frame_count[trackID] = 1
-            
-            # output tracker information
-            print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
-            # visualize 
-            color = Object_colors[int(track.track_id) % len(Object_colors)]
-            if visualize:
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-                cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
-        
-        print('At the current frame: {} cars, {} busses, {} trucks'.format(cars, busses, trucks))
-        if visualize:
-            cv2.imshow("output", frame)
-        fps = 1.0 / (time.time() - start_time)
-        fpsSum += fps
-        fpsCounter += 1
-        print("FPS: %.2f" % fps)
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
-    # timing in seconds 
-    elif time.time() - total_start_time  > 60: 
-        current = time.time()
-        print(wait_frame_count)
-        wait_time_count = {}
-        for k in wait_frame_count:                
-            waittime = wait_frame_count[k] / fpscv2
-            if waittime > 0:
-                wait_time_count[k]  = waittime
-        print(wait_time_count)
-        # average wait time
-        average_wait_time = sum(wait_time_count.values()) / len(wait_time_count)
-        print('The average wait time is: {}'.format(str(average_wait_time)))
-        print(f"fps: {fpscv2}")
-        # publish to IoT
-        message = {} 
-        message['fps'] = fpscv2
-        message['waittime'] = average_wait_time
-        message['busses'] = busses
-        message['cars'] = cars
-        message['trucks'] = trucks
-        # cityType 
-        # city 
-        messageJson = json.dumps(message)
-        myAWSIoTMQTTClient.publish(topic, messageJson, 1)
-        print('Published to IoT, DELAY 5 seconds')
-        time.sleep(5)
-        # reset values 
-        busses = 0
-        trucks = 0
-        cars = 0
-        start = True
-        total_start_time = 0
-    else:
-        print('Restarting the video')
+    try:
+        vid = cv2.VideoCapture(int(video_path))
+    except:
         vid = cv2.VideoCapture(video_path)
-vid.release()
-cv2.destroyAllWindows()
+    out = None
+    while vid.isOpened():
+        # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+        return_value, frame = vid.read()
+        if return_value:
+            start_time = time.time()
+            detections = Object_detector.detect(frame)
+            if WarmUpCount < 9:
+                WarmUpCount += 1 
+                continue
+            if start:
+                total_start_time = time.time() 
+                start = False
+            boxes = [] 
+            labels = []
+            scores = []
+
+            for obj in detections:
+                if obj['label'] in allowed_classes and obj['score'] > 0.5:
+                    [(xmin,ymin),(xmax,ymax)] = obj['bbox']
+                    h = ymax - ymin
+                    w = xmax - xmin
+                    boxes.append((xmin,ymin, w, h))
+                    labels.append(obj['label'])
+                    scores.append(obj['score'])
+
+            boxes = np.array(boxes)
+            labels = np.array(labels)
+            scores = np.array(scores)
+
+            # Start non max supression 
+            features = encoder(frame, boxes)
+            detections = [Detection(box, score, label, feature) for box, score, label, feature in zip(boxes, scores, labels, features)]
+            boxs = np.array([d.tlwh for d in detections])
+            scores = np.array([d.confidence for d in detections])
+        # run non-maxima supression
+            indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+            detections = np.array([detections[i] for i in indices])       
+            # Call the tracker
+            tracker.predict()
+            tracker.update(detections)
+
+            # update tracks
+            cars = 0
+            trucks = 0
+            busses = 0
+            print('Objects being tracked: {}'.format(str(len(tracker.tracks))))        
+            for track in tracker.tracks:
+                if not track.is_confirmed() or track.time_since_update > 1:
+                    continue 
+                bbox = track.to_tlbr()
+                class_name = track.get_class()
+                if class_name == 'car':
+                    cars += 1
+                elif class_name == 'truck':
+                    trucks += 1
+                elif class_name == 'bus':
+                    busses += 1
+
+                #counting frames for each track
+                trackID=str(track.track_id)
+                if trackID in wait_frame_count.keys():
+                    wait_frame_count[trackID] += 1   
+                else:
+                    wait_frame_count[trackID] = 1
+                
+                # output tracker information
+                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+                # visualize 
+                color = Object_colors[int(track.track_id) % len(Object_colors)]
+                if visualize:
+                    cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+                    cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+                    cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+            
+            print('At the current frame: {} cars, {} busses, {} trucks'.format(cars, busses, trucks))
+            if visualize:
+                cv2.imshow("output", frame)
+            fps = 1.0 / (time.time() - start_time)
+            fpsSum += fps
+            fpsCounter += 1
+            print("FPS: %.2f" % fps)
+            if cv2.waitKey(1) & 0xFF == ord('q'): break
+        # timing in seconds 
+        elif time.time() - total_start_time  > 60: 
+            current = time.time()
+            print(wait_frame_count)
+            wait_time_count = {}
+            for k in wait_frame_count:                
+                waittime = wait_frame_count[k] / fpscv2
+                if waittime > 0:
+                    wait_time_count[k]  = waittime
+            print(wait_time_count)
+            # average wait time
+            average_wait_time = sum(wait_time_count.values()) / len(wait_time_count)
+            print('The average wait time is: {}'.format(str(average_wait_time)))
+            print(f"fps: {fpscv2}")
+            # publish to IoT
+            message = {} 
+            message['fps'] = fpscv2
+            message['waittime'] = average_wait_time
+            message['busses'] = busses
+            message['cars'] = cars
+            message['trucks'] = trucks
+            # cityType 
+            # city 
+            messageJson = json.dumps(message)
+            myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+            print('Published to IoT, DELAY 5 seconds')
+            time.sleep(5)
+            # reset values 
+            busses = 0
+            trucks = 0
+            cars = 0
+            start = True
+            total_start_time = 0
+            vid = cv2.VideoCapture(video_path)
+        else:
+            print('Restarting the video')
+            vid = cv2.VideoCapture(video_path)
+    vid.release()
+    cv2.destroyAllWindows()
 
