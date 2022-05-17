7ad73ed01c313b0de524b7e160cdc10f3ab846c6
Abdullatif <arashdan@amazon.com>
2022-05-17 15:29:13 +0300

    docker

--- pytorchYolo-container/runDocker.sh
+++ pytorchYolo-container/runDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker build -t detector .
 sudo xhost +
-sudo docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it --rm --runtime nvidia --network host -v /home/trafficpollution/repo/SCEPollution:/homE detector  # <--- Here
+sudo docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it --rm --runtime nvidia --network host -v /home/trafficpollution/repo/SCEPollution:/home detector  # <--- Here
